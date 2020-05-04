6432a755741e0047e9051316e0d26b8ffc7078a2
Maeriel Benedicto <N00150817@student.iadt.ie>
2020-05-04 21:52:56 +0100

    refresh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-      // if ($this->app->environment() !== 'production') {
-      //     $this->app->register(IdeHelperServiceProvider::class);
-      // }
+      
     }
 
     /**
