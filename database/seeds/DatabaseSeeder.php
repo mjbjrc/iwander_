f79d986cf4155e728f3dd948af1a2f5ec6dc0ad7
Maeriel Benedicto <N00150817@student.iadt.ie>
2020-05-09 21:31:29 +0100

    update seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,8 +24,9 @@ class DatabaseSeeder extends Seeder
       $this->call(BusinessHoursTableSeeder::class);
       $this->call(AttractionCategorySeeder::class);
       $this->call(AttractionKeywordTableSeeder::class);
-      $this->call(ItinerariesTableSeeder::class);
-      $this->call(EventsTableSeeder::class);
-      $this->call(EventsItinerariesTableSeeder::class);
+      //comment for heroku 
+      // $this->call(ItinerariesTableSeeder::class);
+      // $this->call(EventsTableSeeder::class);
+      // $this->call(EventsItinerariesTableSeeder::class);
     }
 }
